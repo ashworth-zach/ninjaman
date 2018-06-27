acbe6e2412cc8902b6bfee60faacfa7900c10948
Zach Ashworth <zashworth2@gmail.com>
2018-06-26 21:58:58 -0500

    ninjaman
    with worldgen and enemies

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var worldDict = {
     3: 'onigiri'
 }
 function genWorld() {
-    world.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    world.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); // top wall
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 20; i++) { //amount of rows
         var arr = [];
         for (var j = 0; j < world[0].length; j++) {
             var random = Math.floor((Math.random() * 4) + 1) - 1;
@@ -19,13 +19,13 @@ function genWorld() {
     }
     for (var x = 0; x < world.length; x++) {
         if (world[x][0] == world[x][0]) {
-            world[x][0] = 1;
+            world[x][0] = 1; //left side wall
         }
-        if (world[x][9] == world[x][9]) {
-            world[x][9] = 1;
+        if (world[x][19] == world[x][19]) {
+            world[x][19] = 1; //right side wall
         }
     }
-    world.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    world.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); //bottom wall
 }
 function drawWorld() {
     out = '';
@@ -42,58 +42,136 @@ function drawWorld() {
 }
 genWorld();
 drawWorld();
+
 var ninjaman = {
     x: 1,
     y: 1
+}
+var bluey = {
+    x:15,
+    y:1
+}
+var red = {
+    x:15,
+    y:12
+}
+function drawBluey(){
+    document.getElementById('bluey').style.top = bluey.y * 40 + 'px'
+    document.getElementById('bluey').style.left = bluey.x * 40 + 'px'
 }
 function drawNinjaman() {
     document.getElementById('ninjaman').style.top = ninjaman.y * 40 + 'px'
     document.getElementById('ninjaman').style.left = ninjaman.x * 40 + 'px'
 }
+function drawRed(){
+    document.getElementById('red').style.top = red.y * 40 + 'px'
+    document.getElementById('red').style.left = red.x * 40 + 'px'
+}
+function chaseKill(){
+    
+    if (ninjaman.x > bluey.x){
+        bluey.x++
+        if (world[bluey.y][bluey.x] == 1) {
+            bluey.x--//left
+        }
+    }
+    if (ninjaman.x < bluey.x){
+        bluey.x--
+        if (world[bluey.y][bluey.x] == 1) {
+            bluey.x++//right
+        }
+    }
+    if (ninjaman.y > bluey.y){
+        bluey.y++
+        if (world[bluey.y][bluey.x] == 1) {
+            bluey.y--//down
+        }
+    }
+    if (ninjaman.y < bluey.y){
+        bluey.y--
+        if (world[bluey.y][bluey.x] == 1) {
+            bluey.y++//up
+        }
+    }
+    if (ninjaman.y == bluey.y && ninjaman.x == bluey.x){
+        document.getElementById('score').innerHTML = "the score is: " + score + ", But you lost so hit F5 to restart";
+    }
+    if (ninjaman.x > red.x){
+        red.x++
+        if (world[red.y][red.x] == 1) {
+            red.x--//left
+        }
+    }
+    if (ninjaman.x < red.x){
+        red.x--
+        if (world[red.y][red.x] == 1) {
+            red.x++//right
+        }
+    }
+    if (ninjaman.y > red.y){
+        red.y++
+        if (world[red.y][red.x] == 1) {
+            red.y--//down
+        }
+    }
+    if (ninjaman.y < red.y){
+        red.y--
+        if (world[red.y][red.x] == 1) {
+            red.y++//up
+        }
+    }
+    if (ninjaman.y == red.y && ninjaman.x == red.x){
+        document.getElementById('score').innerHTML = "the score is: " + score + ", But you lost so hit F5 to restart";
+    }
+}
 document.onkeydown = function (e) {
     //console.log(e.keyCode);
+    drawBluey();
+    drawRed();
+    chaseKill();
     if (e.keyCode == 37) {
         ninjaman.x--
         if (world[ninjaman.y][ninjaman.x] == 1) {
-            ninjaman.x++
+            ninjaman.x++//right
         }
     }
     if (e.keyCode == 39) {
         ninjaman.x++
         if (world[ninjaman.y][ninjaman.x] == 1) {
-            ninjaman.x--
+            ninjaman.x--//left
         }
     }
     if (e.keyCode == 38) {
         ninjaman.y--
         if (world[ninjaman.y][ninjaman.x] == 1) {
-            ninjaman.y++
+            ninjaman.y++//up
         }
     }
     if (e.keyCode == 40) {
         ninjaman.y++
         if (world[ninjaman.y][ninjaman.x] == 1) {
-            ninjaman.y--
+            ninjaman.y--//down
         }
     }
     if (world[ninjaman.y][ninjaman.x] == 2) {
         world[ninjaman.y][ninjaman.x] = 0;
-        score = score + 10;
+        score = score + 10; //SUSHI 10 Pts
         drawNinjaman();
         drawWorld();
         document.getElementById('score').innerHTML = "the score is: " + score;
     }
     else if (world[ninjaman.y][ninjaman.x] == 3) {
         world[ninjaman.y][ninjaman.x] = 0
-        score = score + 5;
+        score = score + 5; //ONIGIRI 5 Pts
         drawNinjaman();
         drawWorld();
         document.getElementById('score').innerHTML = "the score is: " + score;
     }
     else if (world[ninjaman.y][ninjaman.x] == 0) {
         world[ninjaman.y][ninjaman.x] = 0;
-        drawNinjaman();
+        drawNinjaman(); //BLANK SPACE
         drawWorld();
     }
+
 }
 //keep score sushi-10pts onigiri -5pts
